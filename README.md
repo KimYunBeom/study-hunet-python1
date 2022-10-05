df143d9ace3e9cd2e4a2b14423b33be5b2b90c6d
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-10-05 14:47:01 +0900

    파이썬의 숫자와 문자 이해하기

---
+++ README.md
@@ -0,0 +1,25 @@
+# study-hunet-python1
+
+## title
+
+내 일을 바꾸는 파이썬 업무 자동화
+
+## installation
+
+[Python](https://www.python.org)
+
+## version check
+
+```
+$ py --version
+Python 3.10.7
+
+$ pip --version
+pip 22.2.2 from C:\Users\user\AppData\Local\Programs\Python\Python310\lib\site-packages\pip (python 3.10)
+```
+
+## Extension
+
+```
+$ C:/Users/user/AppData/Local/Programs/Python/Python310/python.exe -m pip install -U black
+```
---
+++ hello.py
@@ -0,0 +1,40 @@
+# 숫자 연산
+"""
+data1 = 1 + 2
+print(data1)
+
+data2 = 5 - 3
+print(data2)
+
+data3 = 2 * 5
+print(data3)
+
+data4 = 2**5  # 제곱
+print(data4)
+
+data5 = 1 / 10  # 나누기
+print(data5)
+
+data6 = 23 // 10  # 몫
+print(data6)
+
+data7 = 23 % 10  # 나머지
+print(data7)
+"""
+
+# 문자열 처리
+data8 = "Hello World"  # '," 둘 다 가능
+data9 = """Hello World"""  # ''',""" 둘 다 가능
+
+# print(data8)
+# print(data9)
+
+# 특수문자 및 개행
+data10 = "Hello \n ' \" World"
+"""
+print(data10)
+"""
+
+# 범위 지정
+print(data10[0:5])  # [index:index-1] result : Hello
+print(data10[0])
