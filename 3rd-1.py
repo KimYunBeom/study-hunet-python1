45d85c6ec9b386023cfc3977c1de376533fc7bde
idealful <idealful@gmail.com>
2022-10-06 10:33:51 +0900

    생각의 표현, 조건문 이해하기

---
+++ 3rd-1.py
@@ -0,0 +1,57 @@
+# String
+
+"""
+status = "성공"
+
+if status == "성공":
+    print("성공")
+elif status == "실패":
+    print("실패")
+else:
+    print("그외")
+"""
+
+# not String
+
+"""
+# status = 0
+# status = 1
+
+# status = []
+# status = ["a"]
+
+# status = ()
+# status = "a"
+
+# status = {}
+# status = {"a": "data"}
+
+status = "성공"
+if status == "성공" and status == "test":
+if status == "성공" or status == "test":
+    print("성공")
+else:
+    print("실패")
+"""
+
+# in Array
+
+"""
+status = [1, 2, 3, 4, 5]
+
+# if 1 in status:
+# if 6 not in status:
+    print("성공")
+else:
+    print("실패")
+"""
+
+# in String
+
+status = "Hello World"
+
+# if "H" in status:
+if "a" in status:
+    print("성공")
+else:
+    print("실패")
